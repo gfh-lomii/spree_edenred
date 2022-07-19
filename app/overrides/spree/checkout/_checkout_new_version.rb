ccacac2c1038d800f680a65b2fd1214f84a384c4
rmartinezapp <robert.martinez.app@gmail.com>
2022-07-19 05:45:46 -0400

    add Edenred to new checkout

---
+++ app/overrides/spree/checkout/_checkout_new_version.rb
@@ -0,0 +1,38 @@
+Deface::Override.new(
+  virtual_path: 'spree/checkout/_checkout_new_version',
+  name: 'edenred_payment_method',
+  insert_top: '[data-hook="payment_methods_list"]',
+  text: %{
+    <% payment_method = Spree::PaymentMethod.active.available_on_front_end.find_by_type("Spree::PaymentMethod::Edenred") %>
+        <% if payment_method && @ld_client.variation("edenred-payment", @ld_user, true) %>
+          <label class="d-block" for="<%= dom_id(payment_method) %>">
+            <div class="card-lomi mb-3">
+              <div class="d-flex justify-content-between align-items-center px-1">
+                <div class="d-flex">
+                  <div class="icon-lomi boton-rounded mr-3 shadow2-lomi justify-content-center align-items-center" style="width: 41px; height: 41px;">
+                    <%= image_tag main_app.url_for(payment_method&.image&.url(:small) || 'icon.png'), class: 'w-100 d-block my-1 rounded-circle' %>
+                  </div>
+                  <div>
+                    <p class="titulo-l-semibold color-dark-blue-lomi mb-1"><%= payment_method.name %></strong></label>
+                    <p class="mb-0 texto-s color-grey-lomi"><%= payment_method.description %></p>
+                  </div>
+                </div>
+                <input type="radio"
+                      id="<%= dom_id(payment_method) %>"
+                      name="method"
+                      data-action='checkout#selectPaymentMethod'
+                      value="<%= payment_method.id %>"
+                      data-type="<%= payment_method.type %>"
+                      data-title="<%= payment_method.name %>"
+                      data-description="<%= payment_method.description %>"
+                      data-icon="<%= main_app.url_for(payment_method&.image&.url(:small) || 'icon.png') %>"
+                      <%= 'disabled' if @order.any_item_with_alcohol? %>>
+              </div>
+            </div>
+            <% if @order.any_item_with_alcohol? %>
+              <p>Compras con alcohol incluido no son permitidas por Sodexo.</p>
+            <% end %>
+          </label>
+        <% end %>
+  }
+)
